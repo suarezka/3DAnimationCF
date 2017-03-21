ba5bc217b94464a101491e9a834c7a9d76c3b69f
Kaye Suarez <suarezka@mail.gvsu.edu>
2017-03-20 23:52:31 -0400

    New Project compiled Lighting example + Project 1 - need to add lighting vshader + fshader

---
+++ Pineapple.js
@@ -0,0 +1,152 @@
+/**
+ * Created by kaye on 2/25/17.
+ */
+/**
+ * Created by Hans Dulimarta on 2/16/17.
+ */
+class Pineapple {
+    constructor (gl) {
+        //Yellow + Brownish
+        let baseColor = vec3.fromValues(1.0, 1.0, 0.0);
+        let baseColor2 = vec3.fromValues(0.803922, 0.521569, 0.247059);
+
+        //Green + Dark Green
+        let crownColor = vec3.fromValues(0.0, 1.0, 0.0);
+        let crownColor2 = vec3.fromValues(0.0, 0.392157, 0.0);
+
+        //Pipe Color
+        let pipeColor = vec3.fromValues(0.117647, 0.564706, 1);
+        let windowColor = vec3.fromValues(0.529412, 0.807843, 0.980392);
+        let windowColor2 = vec3.fromValues(0.429412, 0.707843, 0.940392);
+
+        // Door Color
+        let doorColor = vec3.fromValues(0.745098, 0.745098, 0.745098);
+
+
+        this.base = new Cylinder(gl, 0.20, 0.15, 0.2, 16, baseColor, baseColor2);
+        this.top = new Cylinder(gl, 0.20, 0.15, 0.2, 16, baseColor, baseColor2);
+        this.c1 = new Cone(gl, 0.12, 0.2, 15, 1, crownColor, crownColor2);
+        this.c2 = new Cone(gl, 0.11, 0.2, 15, 1, crownColor, crownColor2);
+        this.c3 = new Cone(gl, 0.1, 0.2, 15, 1, crownColor, crownColor2);
+        this.pipe1 = new Cylinder(gl, 0.01, 0.01, 0.2, 15, pipeColor, pipeColor);
+        this.pipe2 = new Cylinder(gl, 0.05, 0.01, 0.1, 15, pipeColor, pipeColor);
+        this.window1 = new Ring(gl, 0.05, 0.025, 0.1, 10, 4, windowColor, windowColor2);
+        this.window2 = new Ring(gl, 0.05, 0.025, 0.1, 10, 4, windowColor, windowColor2);
+        this.door = new Cylinder(gl, 0.06, 0.06, 0.1, 10, pipeColor, pipeColor);
+        this.door2 = new Cube(gl, 0.12, 4, pipeColor, pipeColor, pipeColor);
+
+
+        //Repositioning for Base and Top
+        let moveBaseUp = vec3.fromValues(0, 0, 0.1);
+        this.baseTransform = mat4.create();
+        mat4.fromTranslation(this.baseTransform, moveBaseUp);
+
+        let moveUp = vec3.fromValues (0, 0, 0.3);
+        this.topTransform = mat4.create();
+        mat4.rotateY(this.topTransform, this.topTransform, Math.PI);
+        let moveUpTop = mat4.fromTranslation(mat4.create(), moveUp);
+        //crownTransform = moveUpCrown * crownTransform
+        mat4.multiply (this.topTransform, moveUpTop, this.topTransform);
+
+        //Side Pipe
+        let move = vec3.fromValues (0, 0.15, 0.32);
+        this.pipe1Transform = mat4.create();
+        mat4.rotateX(this.pipe1Transform, this.pipe1Transform, Math.PI / 2);
+        let moveSide = mat4.fromTranslation(mat4.create(), move);
+        mat4.multiply(this.pipe1Transform, moveSide, this.pipe1Transform);
+
+        this.pipe2Transform = mat4.create();
+        mat4.fromTranslation(this.pipe2Transform, vec3.fromValues(0, 0.23, 0.36));
+
+        // Windows
+        let moveWindow = vec3.fromValues(0.13, -0.07, 0.28);
+        this.windowTransform = mat4.create();
+        mat4.rotateY(this.windowTransform, this.windowTransform, Math.PI/2);
+        let moveUpWindow = mat4.fromTranslation(mat4.create(), moveWindow);
+        mat4.multiply(this.windowTransform, moveUpWindow, this.windowTransform);
+
+        let moveWindow2 = vec3.fromValues(0.13, 0.12, 0.15);
+        this.window2Transform = mat4.create();
+        mat4.rotateY(this.window2Transform, this.window2Transform, Math.PI/2);
+        let moveUpWindow2 = mat4.fromTranslation(mat4.create(), moveWindow2);
+        mat4.multiply(this.window2Transform, moveUpWindow2, this.window2Transform);
+
+        // Door
+        let moveDoor = vec3.fromValues(0.14, 0, 0.11);
+        this.doorTransform = mat4.create();
+        mat4.rotateY(this.doorTransform, this.doorTransform, Math.PI/2);
+        let moveDoorUp = mat4.fromTranslation(mat4.create(), moveDoor);
+        mat4.multiply(this.doorTransform, moveDoorUp, this.doorTransform);
+
+        let moveDoor2 = vec3.fromValues(0.17, 0, 0.07);
+        this.door2Transform = mat4.create();
+        mat4.scale(this.door2Transform, this.door2Transform, vec3.fromValues(0.2, 1, 1));
+        let moveDoorUp2 = mat4.fromTranslation(mat4.create(), moveDoor2);
+        mat4.multiply(this.door2Transform, moveDoorUp2, this.door2Transform);
+
+        //Repositioning C1 of Cones
+        this.c1Transform = mat4.create();
+        mat4.rotateY(this.c1Transform, this.c1Transform, Math.PI);
+        let moveUp2 = vec3.fromValues(0, 0, 0.5);
+        let moveUpC1 = mat4.fromTranslation(mat4.create(), moveUp2);
+        mat4.multiply (this.c1Transform, moveUpC1, this.c1Transform);
+
+
+        //C2
+        this.c2Transform = mat4.create();
+        mat4.rotateY(this.c2Transform, this.c2Transform, Math.PI);
+        let moveUp3 = vec3.fromValues(0, 0, 0.6);
+        let moveUpC2 = mat4.fromTranslation(mat4.create(), moveUp3);
+        mat4.multiply (this.c2Transform, moveUpC2, this.c2Transform);
+
+
+        //C3
+        this.c3Transform = mat4.create();
+        mat4.rotateY(this.c3Transform, this.c3Transform, Math.PI);
+        let moveUp4 = vec3.fromValues(0, 0, 0.7);
+        let moveUpC3 = mat4.fromTranslation(mat4.create(), moveUp4);
+        mat4.multiply (this.c3Transform, moveUpC3, this.c3Transform);
+
+
+
+
+        this.tmp = mat4.create();
+    }
+
+    draw (vertexAttr, colorAttr, modelUniform, coordFrame) {
+        mat4.mul (this.tmp, coordFrame, this.baseTransform);
+        this.base.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.topTransform);
+        this.top.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.pipe1Transform);
+        this.pipe1.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.pipe2Transform);
+        this.pipe2.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.windowTransform);
+        this.window1.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.window2Transform);
+        this.window2.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.doorTransform);
+        this.door.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.door2Transform);
+        this.door2.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.c1Transform);
+        this.c1.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.c2Transform);
+        this.c2.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+        mat4.mul (this.tmp, coordFrame, this.c3Transform);
+        this.c3.draw(vertexAttr, colorAttr, modelUniform, this.tmp);
+
+
+    }
+}
---
+++ gl-main.js
@@ -0,0 +1,303 @@
+/**
+ * Created by Hans Dulimarta on 1/31/17.
+ */
+
+var gl;
+var glCanvas, textOut;
+var orthoProjMat, persProjMat, viewMat, topViewMat, sideViewMat, ringCF;
+var pineappleCF, statueCF, rockCF, floorCF;
+var axisBuff, tmpMat;
+var globalAxes;
+var currSelection = 0;
+var currObjName;
+
+/* Vertex shader attribute variables */
+var posAttr, colAttr;
+
+/* Shader uniform variables */
+var projUnif, viewUnif, modelUnif;
+
+const IDENTITY = mat4.create();
+var coneSpinAngle;
+var obj, obj2, obj3, floor;
+var shaderProg;
+
+function main() {
+    glCanvas = document.getElementById("gl-canvas");
+    textOut = document.getElementById("msg");
+    gl = WebGLUtils.setupWebGL(glCanvas, null);
+    axisBuff = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, axisBuff);
+    window.addEventListener("resize", resizeHandler, false);
+    window.addEventListener("keypress", keyboardHandler, false);
+    let menu = document.getElementById("menu");
+    menu.addEventListener("change", menuSelected);
+
+    ShaderUtils.loadFromFile(gl, "vshader.glsl", "fshader.glsl")
+        .then(prog => {
+            shaderProg = prog;
+            gl.useProgram(prog);
+            gl.clearColor(0, 163/255, 230/255, 1);
+            gl.enable(gl.DEPTH_TEST);
+            /* enable hidden surface removal */
+            //gl.enable(gl.CULL_FACE);     /* cull back facing polygons */
+            //gl.cullFace(gl.BACK);
+            posAttr = gl.getAttribLocation(prog, "vertexPos");
+            colAttr = gl.getAttribLocation(prog, "vertexCol");
+            projUnif = gl.getUniformLocation(prog, "projection");
+            viewUnif = gl.getUniformLocation(prog, "view");
+            modelUnif = gl.getUniformLocation(prog, "modelCF");
+            gl.enableVertexAttribArray(posAttr);
+            gl.enableVertexAttribArray(colAttr);
+            orthoProjMat = mat4.create();
+            persProjMat = mat4.create();
+            viewMat = mat4.create();
+            topViewMat = mat4.create();
+            sideViewMat = mat4.create();
+            ringCF = mat4.create();
+            pineappleCF = mat4.create();
+            statueCF = mat4.create();
+            rockCF = mat4.create();
+            floorCF = mat4.create();
+            tmpMat = mat4.create();
+            mat4.lookAt(viewMat,
+                vec3.fromValues(2, 2, 2), /* eye */
+                vec3.fromValues(0, 0, 0), /* focal point */
+                vec3.fromValues(0, 0, 1));
+            /* up */
+            mat4.lookAt(topViewMat,
+                vec3.fromValues(0, 0, 2),
+                vec3.fromValues(0, 0, 0),
+                vec3.fromValues(0, 1, 0));
+            mat4.lookAt(sideViewMat,
+                vec3.fromValues(2, 0, 0),
+                vec3.fromValues(0, 0, 0),
+                vec3.fromValues(0, 0, 1));
+
+            gl.uniformMatrix4fv(modelUnif, false, pineappleCF);
+            gl.uniformMatrix4fv(modelUnif, false, statueCF);
+            gl.uniformMatrix4fv(modelUnif, false, rockCF);
+            gl.uniformMatrix4fv(modelUnif, false, ringCF);
+
+            obj = new Pineapple(gl);
+            obj2 = new SquidwardHouse(gl);
+            obj3 = new PatrickHouse(gl);
+            floor = new Floor(gl);
+            globalAxes = new Axes(gl);
+
+
+           // mat4.rotateX(pineappleCF, pineappleCF, Math.PI);
+            mat4.rotateX(ringCF, ringCF, -Math.PI / 2);
+            coneSpinAngle = 10;
+            resizeHandler();
+            render();
+        });
+}
+
+function resizeHandler() {
+    glCanvas.width = window.innerWidth;
+    glCanvas.height = 0.9 * window.innerHeight;
+    if (glCanvas.width > glCanvas.height) { /* landscape */
+        let ratio = 2 * glCanvas.height / glCanvas.width;
+        console.log("Landscape mode, ratio is " + ratio);
+        mat4.ortho(orthoProjMat, -3, 3, -3 * ratio, 3 * ratio, -5, 5);
+        mat4.perspective(persProjMat,
+            Math.PI / 3, /* 60 degrees vertical field of view */
+            1 / ratio, /* must be width/height ratio */
+            1, /* near plane at Z=1 */
+            20);
+        /* far plane at Z=20 */
+    } else {
+        alert("Window is too narrow!");
+    }
+
+}
+
+function keyboardHandler(event) {
+    const transXpos = mat4.fromTranslation(mat4.create(), vec3.fromValues(1, 0, 0));
+    const transXneg = mat4.fromTranslation(mat4.create(), vec3.fromValues(-1, 0, 0));
+    const transYpos = mat4.fromTranslation(mat4.create(), vec3.fromValues(0, 1, 0));
+    const transYneg = mat4.fromTranslation(mat4.create(), vec3.fromValues(0, -1, 0));
+    const transZpos = mat4.fromTranslation(mat4.create(), vec3.fromValues(0, 0, 1));
+    const transZneg = mat4.fromTranslation(mat4.create(), vec3.fromValues(0, 0, -1));
+
+    const rotateZccw = mat4.fromZRotation(mat4.create(), coneSpinAngle * Math.PI/180.0);
+    const rotateZcw = mat4.fromZRotation(mat4.create(), - (coneSpinAngle * Math.PI/180.0));
+    const rotateXccw = mat4.fromXRotation(mat4.create(), coneSpinAngle * Math.PI/180.0);
+    const rotateXcw = mat4.fromXRotation(mat4.create(), - (coneSpinAngle * Math.PI/180.0));
+    const rotateYccw = mat4.fromYRotation(mat4.create(), coneSpinAngle * Math.PI/180.0);
+    const rotateYcw = mat4.fromYRotation(mat4.create(), - (coneSpinAngle * Math.PI/180.0));
+
+    let objCF = mat4.create();
+
+    switch (currSelection) {
+        case 0:
+            currObjName = document.getElementById("house0").innerText;
+            objCF = pineappleCF;
+            break;
+        case 1:
+            currObjName = document.getElementById("house1").innerText;
+            objCF = statueCF;
+            break;
+        case 2:
+            currObjName = document.getElementById("house2").innerText;
+            objCF = rockCF;
+            break;
+    }
+
+    switch (event.key) {
+        case "x":
+            mat4.multiply(objCF, transXneg, objCF);  // ringCF = Trans * ringCF
+            break;
+        case "X":
+            mat4.multiply(objCF, transXpos, objCF);  // ringCF = Trans * ringCF
+            break;
+        case "y":
+            mat4.multiply(objCF, transYneg, objCF);  // ringCF = Trans * ringCF
+            break;
+        case "Y":
+            mat4.multiply(objCF, transYpos, objCF);  // ringCF = Trans * ringCF
+            break;
+        case "z":
+            mat4.multiply(objCF, transZneg, objCF);  // ringCF = Trans * ringCF
+            break;
+        case "Z":
+            mat4.multiply(objCF, transZpos, objCF);  // ringCF = Trans * ringCF
+            break;
+        case "l":
+            mat4.multiply(objCF, objCF, rotateXccw);
+            break;
+        case "r":
+            mat4.multiply(objCF, objCF, rotateXcw);
+            break;
+        case "u":
+            mat4.multiply(objCF, objCF, rotateYcw);
+            break;
+        case "d":
+            mat4.multiply(objCF, objCF, rotateYccw);
+            break;
+        case "c":
+            mat4.multiply(objCF, objCF, rotateZccw);
+            break;
+        case "C":
+            mat4.multiply(objCF, objCF, rotateZcw);
+            break;
+        case "+":
+            mat4.scale(objCF, objCF, vec3.fromValues(1.05, 1.05, 1.05));
+            break;
+        case "-":
+            mat4.scale(objCF, objCF, vec3.fromValues(0.75, 0.75, 0.75));
+            break;
+    }
+    textOut.innerHTML = currObjName + " origin (" + objCF[12].toFixed(1) + ", "
+        + objCF[13].toFixed(1) + ", "
+        + objCF[14].toFixed(1) + ")";
+}
+
+function render() {
+    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+    draw3D();
+    drawTopView();
+    /* looking at the XY plane, Z-axis points towards the viewer */
+    //coneSpinAngle += 1;  /* add 1 degree */
+    requestAnimationFrame(render);
+}
+
+function drawScene() {
+    let rowNum = document.getElementById("house-row").valueAsNumber;
+    let num = document.getElementById("house-number").valueAsNumber;
+
+    floor.draw(posAttr, colAttr, modelUnif, floorCF);
+
+    //Draw Multiple Pineapple Houses
+    if (typeof obj !== 'undefined') {
+        var xPos;
+        var yPos;
+        var x = 0.5;
+        for (let j = 0; j < rowNum; j++) {
+            let row = j * x;
+            xPos = 0;
+            yPos = 1.5 + row;
+
+            for (let k = 0; k < num; k++) {
+                mat4.fromTranslation(tmpMat, vec3.fromValues(xPos, yPos, 0));
+                mat4.multiply(tmpMat, pineappleCF, tmpMat);   // tmp = ringCF * tmpMat
+                obj.draw(posAttr, colAttr, modelUnif, tmpMat);
+                xPos -= 0.5;
+                yPos -= 0.5;
+            }
+        }
+    }
+
+    //Draw Multiple Squidward Houses
+    if (typeof obj2 !== 'undefined') {
+        var xPos;
+        var yPos;
+        var x = 0.5;
+        for (let j = 0; j < rowNum; j++) {
+            let row = j * x;
+            xPos = 0;
+            yPos = 0 + row;
+
+            for (let k = 0; k < num; k++) {
+                mat4.fromTranslation(tmpMat, vec3.fromValues(xPos, yPos, 0));
+                mat4.multiply(tmpMat, statueCF, tmpMat);   // tmp = ringCF * tmpMat
+                obj2.draw(posAttr, colAttr, modelUnif, tmpMat);
+                xPos -= 0.5;
+                yPos -= 0.5;
+            }
+        }
+    }
+
+    //Draw Multiple Patrick Houses
+    if (typeof obj3 !== 'undefined') {
+        var xPos;
+        var yPos;
+        var x = 0.5;
+        for (let j = 0; j < rowNum; j++) {
+            let row = j * x;
+            xPos = 0;
+            yPos = -1.5 + row;
+
+            for (let k = 0; k < num; k++) {
+                mat4.fromTranslation(tmpMat, vec3.fromValues(xPos, yPos, 0));
+                mat4.multiply(tmpMat, rockCF, tmpMat);   // tmp = ringCF * tmpMat
+                obj3.draw(posAttr, colAttr, modelUnif, tmpMat);
+                xPos -= 0.5;
+                yPos -= 0.5;
+            }
+        }
+    }
+}
+
+function draw3D() {
+    /* We must update the projection and view matrices in the shader */
+    gl.uniformMatrix4fv(projUnif, false, persProjMat);
+    gl.uniformMatrix4fv(viewUnif, false, viewMat);
+    gl.viewport(0, 0, glCanvas.width / 2, glCanvas.height);
+    drawScene();
+}
+
+function drawTopView() {
+    /* We must update the projection and view matrices in the shader */
+    gl.uniformMatrix4fv(projUnif, false, orthoProjMat);
+    gl.uniformMatrix4fv(viewUnif, false, topViewMat);
+    gl.viewport(glCanvas.width / 2, 0, glCanvas.width / 2, glCanvas.height);
+    drawScene();
+}
+
+function drawSideView() {
+    /* We must update the projection and view matrices in the shader */
+    gl.uniformMatrix4fv(projUnif, false, orthoProjMat);
+    gl.uniformMatrix4fv(viewUnif, false, sideViewMat);
+    gl.viewport(glCanvas.width / 2, 0, glCanvas.width / 2, glCanvas.height);
+    drawScene();
+}
+
+function menuSelected(ev) {
+    let sel = ev.currentTarget.selectedIndex;
+   // paramGroup[currSelection].hidden = true;
+   // paramGroup[sel].hidden = false;
+    currSelection = sel;
+    console.log("New selection is ", currSelection);
+}
